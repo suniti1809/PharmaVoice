9ca71b8483aab6e98bc04494f79c01dd2a080193
Suniti <suniti2864@gmail.com>
2026-09-17 21:39:55 +0530

    Update main.jsx
    Updated scroll restoration check to use window.history.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+
 import App from './App';
 import { store } from './app/store';
+
 import './styles.css';
 
-if ('scrollRestoration' in history) {
-  history.scrollRestoration = 'manual';
+if ('scrollRestoration' in window.history) {
+  window.history.scrollRestoration = 'manual';
 }
-
-window.scrollTo(0, 0);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
